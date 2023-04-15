f3dff055762503755bc49f7d5ee67b63955e1803
Akanksha <joshiaj33@gmail.com>
2023-04-15 22:32:00 +0530

    navbar edit

--- .history/src/App_20230415210201.js
+++ .history/src/App_20230415210201.js
@@ -1,11 +1,10 @@
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
 import {Home} from './Screens/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
       <Home/>
     </div>
   );
