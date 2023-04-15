77dd1d0d8a9744149c88da91340915fa1ab277b4
Mayank Kirola <69565231+MayankKirola22@users.noreply.github.com>
2023-04-15 23:03:03 +0530

    as

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import {Home} from './Screens/Home';
-
+import {Carbon} from './Screens/carbon';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Carbon/>
     </div>
   );
 }
