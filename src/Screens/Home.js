d397ac9ffbec484de3d6766affd7935c17314d0f
vaishprakhar19 <vaishprakhar19@gmail.com>
2023-04-15 22:35:24 +0530

    jyjy

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,15 +1,18 @@
 import './Home.css';
 
-export function Home(){
-    return(
+export function Home() {
+    return (
         <div className="home">
-            <div id='fade'>
+            <div id='blur'>
                 <div id='home-content'>
+                <div >
                     <p id='intro'>
-                        EcoSavvy is a sustainable development based information and product website which promotes renewable energy sources, such as solar, wind, and hydroelectric power.
+                    EcoSavvy is a sustainable development-based information and product website that promotes renewable energy sources, such as solar, wind, and hydroelectric power. With the current scenario of global energy and resource management, sustainable development is essential for the efficient utilization and management of resources.
                     </p>
-                    <img src='https://github.com/akankshaajoshi/EcoSavvy/blob/mayank/src/Resources/Logo%202.png?raw=true' className='logo2'/>
+                    <div id="about-left"><a href="test" id="about-button">About Us</a> </div></div>
+                    <img src='https://github.com/akankshaajoshi/EcoSavvy/blob/master/src/Resources/logo1.png?raw=true' className='logo2' />
                 </div>
+                
                 <div id='resorce-section'>
                     <div id='res-1' className='resource'>
                         <h1>Solar Energy</h1>
@@ -18,7 +21,7 @@ export function Home(){
                         </p>
                         <a href='test'>Read More</a>
                         <a href='test'>Buy Here</a>
-                        <img src=''/>
+                        <img src='' />
                     </div>
                     <div id='res-2' className='resource'>
                         <h1>Hydro Energy</h1>
@@ -27,7 +30,7 @@ export function Home(){
                             <a href='test'>Read More</a>
                         </p>
                         <a href='test'>Buy Here</a>
-                        <img src=''/>
+                        <img src='' />
                     </div>
                     <div id='res-3' className='resource'>
                         <h1>Wind Energy</h1>
@@ -36,23 +39,24 @@ export function Home(){
                             <a href='test'>Read More</a>
                         </p>
                         <a href='test'>Buy Here</a>
-                        <img src=''/>
+                        <img src='' />
                     </div>
                 </div>
-            </div>
-            <h1>News</h1>
-            <div id='news'>
-                <div class='news-item'>
-                    <img src='fsa' />
-                    lorem ipsum
-                </div>
-                <div class='news-item'>
-                    <img src='saf' />
-                    lorem ipsum
-                </div>
-                <div class='news-item'>
-                    <img src='ca'/>
-                    lorem ipsum
+
+                <h1>News</h1>
+                <div id='news'>
+                    <div class='news-item'>
+                        <img src='fsa' />
+                        lorem ipsum
+                    </div>
+                    <div class='news-item'>
+                        <img src='saf' />
+                        lorem ipsum
+                    </div>
+                    <div class='news-item'>
+                        <img src='ca' />
+                        lorem ipsum
+                    </div>
                 </div>
             </div>
             <div id='footer'>
