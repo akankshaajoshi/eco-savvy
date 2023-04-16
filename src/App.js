41149ff5f937ad4ea4f6fb78eab549266e99134e
Mayank Kirola <69565231+MayankKirola22@users.noreply.github.com>
2023-04-16 06:14:12 +0530

    daad

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {Home} from './Screens/Home';
 import {About} from './Screens/About';
 import {News} from './Screens/News';
 import {Carbon} from './Screens/carbon';
+import {ChatGPT} from './Components/ChatGPT';
 import {Route ,Routes, Navigate} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
@@ -15,6 +17,7 @@ function App() {
           <Route path='/About' element={<About/>}/>
           <Route path='/News' element={<News/>}/> 
           <Route path='/CFC' element={<Carbon/>}/>
+          <Route path='/GPT' element={<ChatGPT/>}/>
         </Routes>
     </div>
   );
