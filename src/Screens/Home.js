b308c107a98081ebb5d6faa710d4270f1c68cdc5
Mayank Kirola <69565231+MayankKirola22@users.noreply.github.com>
2023-04-16 07:07:47 +0530

    afdafd

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,11 +1,25 @@
 import './Home.css';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export function Home() {
     let solar = "Solar energy is radiant light and heat from the Sun that is harnessed using a range of technologies such as solar power to generate electricity, solar thermal energy (including solar water heating), and solar architecture. It is an essential source of renewable energy, and its technologies are broadly characterized as either passive solar or active solar depending on how they capture and distribute solar energy or convert it into solar power. Active solar techniques include the use of photovoltaic systems, concentrated solar power, and solar water heating to harness the energy. Passive solar techniques include orienting a building to the Sun, selecting materials with favorable thermal mass or light-dispersing properties, and designing spaces that naturally circulate air."
     let hydro = "Hydropower , also known as water power, is the use of falling or fast-running water to produce electricity or to power machines. This is achieved by converting the gravitational potential or kinetic energy of a water source to produce power. Hydropower is a method of sustainable energy production. Hydropower is now used principally for hydroelectric power generation, and is also applied as one half of an energy storage system known as pumped-storage hydroelectricity.";
     let wind = "Wind power is the use of wind energy to generate useful work. Historically, wind power was used by sails, windmills and windpumps, but today it is mostly used to generate electricity. This article deals mostly with wind power for electricity generation. Today, wind power is almost completely generated with wind turbines, generally grouped into wind farms and connected to the electrical grid. In 2021, wind supplied over 1800 TWh of electricity, which was over 6% of world electricity and about 2% of world energy. With about 100 GW added during 2021, mostly in China and the United States, global installed wind power capacity exceeded 800 GW. To help meet the Paris Agreement goals to limit climate change, analysts say it should expand much faster - by over 1% of electricity generation per year.";
+    const [newsData, setNewsData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
+    async function getNewsData() {
+        setLoading(true);
+        const resp = await axios.get("https://newsapi.org/v2/everything?q=sustainable development&apiKey=22e7c582a80146a09ca0537125a6608f");
+        setNewsData(resp.data.articles);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getNewsData();
+    }, []);
     return (
         <div className="home">
             <div className='blur'>
@@ -58,20 +72,22 @@ export function Home() {
                 </div>
 
                 <h1>News</h1>
-                {/* <div id='news'>
-                    <div class='news-item'>
-                        <img src='fsa' />
-                        lorem ipsum
-                    </div>
-                    <div class='news-item'>
-                        <img src='saf' />
-                        lorem ipsum
-                    </div>
-                    <div class='news-item'>
-                        <img src='ca' />
-                        lorem ipsum
-                    </div>
-                </div> */}
+                <div id='newsSection'>
+                    {newsData.slice(0,5).map((newsData, index) =>
+                        <div key={index} id='newsbox'>
+                            <a target="_blank" href={newsData.url}>
+                            <div id='news-content'>
+                            <h2 id='news-title'>{newsData.title}</h2>
+                            <p id='news-para'>{newsData.description}</p>
+                            </div>
+                            </a>
+                            <a target="_blank" href={newsData.url}>
+                            <img id='news-img' src={newsData.urlToImage} width="200px"/>
+                            </a>
+                        </div>
+                    )}
+
+                </div>
             </div>
             <div id='footer'>
                 Copyright@EcoSavvy2023
